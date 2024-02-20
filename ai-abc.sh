5246ae07a17423a87588ceb9057417129d1f0a86
dodopontocom <rodolfotiago@gmail.com>
2024-02-19 23:10:22 -0300

    wip: second day

--- ai-abc.sh
+++ ai-abc.sh
@@ -7,34 +7,11 @@ echo "Gerando script Hello World em ${lang} com funcionalidade extra: ${extra}"
 
 case ${lang} in
   "python")
-    echo "
-    ```
-    python
-    print(\"HElloO World\")
-
-    # Funcionalidade extra
-    if '${extra}' == 'data':
-      print(\'Data atual:\', date)
-    ```
-    " > hello_world.py
+    echo ${lang} ${extra} >> hello-world.py
     ;;
   "java")
-    echo "
-    ```
-    java
-    public class HelloWorld {
-      public static void main(String[] args) {
-        System.out.println(\""Hello World!\");
-
-        // Funcionalidade extra
-        if (\"$extra\".equals(\"data\")) {
-          System.out.println(\"Data atual: \" + java.time.LocalDate.now());
-        }
-      }
-    }
-    ```
-    " > hello_world.java
+    echo ${lang} ${extra} >> hello-world.java
     ;;
 esac
 
-echo "Script gerado com sucesso: hello_world.$lang"
+echo "Script gerado com sucesso: hello-world.$lang"
