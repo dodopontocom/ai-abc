be7cabe47c308c8332199e5753bc09da7a8077a5
dodopontocom <rodolfotiago@gmail.com>
2024-02-27 14:09:25 -0300

    "Generate commit message based on diff using Generative AI"

--- ccm.py
+++ ccm.py
@@ -12,10 +12,15 @@ genai.configure(api_key=GOOGLE_API_KEY)
 model = genai.GenerativeModel('gemini-pro')
 
 def generate_git_commit_message():
+
 	git_changes = subprocess.run(["git", "diff", "--cached"], capture_output=True, text=True)
-	return model.generate_content('''You are an expert at creating a github commit message 
-		for a set of changes. Here is a diff of changes we need a commit message for (use around 30 words): ''' 
-		+ str(git_changes))
+	if git_changes.returncode == 0:
+		message = model.generate_content('''You are an expert at creating a github commit message 
+			for a set of changes. Here is a diff of changes we need a commit message for (use around 30 words): ''' 
+			+ str(git_changes))
+	else:
+		message = "failed"
+	return message
 	
 if __name__ == '__main__':
     print(generate_git_commit_message().text)
