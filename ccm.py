3f262f4e5b21ca19669dcf2bb7d723fd5c906722
dodopontocom <rodolfotiago@gmail.com>
2024-02-27 14:52:47 -0300

    Alterações no script de geração de mensagens de commit do GitHub, incluindo: - Importar sys para verificar se o script foi executado com um argumento. - Atualizar a verificação da chave da API. - Atualizar o prompt do modelo para usar frases impessoais.

--- ccm.py
+++ ccm.py
@@ -2,20 +2,26 @@
 
 # create commit message
 
-import os
+import sys, os
 import subprocess
 import google.generativeai as genai
 
-GOOGLE_API_KEY = os.getenv('GEMINI_API_KEY')
-
-genai.configure(api_key=GOOGLE_API_KEY)
+if len(sys.argv) > 1:
+      key = sys.argv[1]
+else:
+      key = os.getenv("GEMINI_API_KEY")
+      if key is None:
+             print(f"Erro: A variável de ambiente GEMINI_API_KEY não está definida.")
+             sys.exit(1)
+            
+genai.configure(api_key=(key))
 model = genai.GenerativeModel('gemini-pro')
 
 def generate_git_commit_message():
-
 	git_changes = subprocess.run(["git", "diff", "--cached"], capture_output=True, text=True)
 	message = model.generate_content('''Voce é um expert em criando mensagens para commites no github 
-		para as mudanças relacionadas. Aqui está um diff das mudanças e preciso de uma mensagem para o commit (usar aproximadamente 30 palavras): ''' 
+		para as mudanças relacionadas. Aqui está um diff das mudanças e preciso de uma mensagem para
+		o commit (usar aproximadamente 30 palavras e usar frases impessoais): ''' 
 		+ str(git_changes))
 	return message
 	
