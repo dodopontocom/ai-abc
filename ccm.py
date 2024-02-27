4b3db4c693fcd6ff40fd81d286fe7521033d91e0
dodopontocom <rodolfotiago@gmail.com>
2024-02-27 14:10:39 -0300

    "Update commit message generation logic to simplify code and improve efficiency."

--- ccm.py
+++ ccm.py
@@ -14,12 +14,9 @@ model = genai.GenerativeModel('gemini-pro')
 def generate_git_commit_message():
 
 	git_changes = subprocess.run(["git", "diff", "--cached"], capture_output=True, text=True)
-	if git_changes.returncode == 0:
-		message = model.generate_content('''You are an expert at creating a github commit message 
-			for a set of changes. Here is a diff of changes we need a commit message for (use around 30 words): ''' 
-			+ str(git_changes))
-	else:
-		message = "failed"
+	message = model.generate_content('''You are an expert at creating a github commit message 
+		for a set of changes. Here is a diff of changes we need a commit message for (use around 30 words): ''' 
+		+ str(git_changes))
 	return message
 	
 if __name__ == '__main__':
